794830eabe75544e33d581c231221209af6b8238
seguronline <miguellllpedroza@gmail.com>
2025-03-22 19:44:23 -0500

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cors from "cors";
+import { MercadoPagoConfig, Preference } from "mercadopago";
+
+// Es recomendable usar variables de entorno para el token
+const client = new MercadoPagoConfig({
+  accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN || 'APP_USR-1649390323222844-031918-5d34f0174bc8cc7a502687b8c3af43ce-1014313313'
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Soy el server :)");
+});
+
+app.post("/create_preference", async (req, res) => {
+  try {
+    const body = {
+      items: [{
+        title: req.body.title,
+        quantity: Number(req.body.quantity),
+        unit_price: Number(req.body.price),
+        currency_id: "COP"
+      }],
+      back_urls: {
+        success: "https://polizasonline.online/sura/",
+        failure: "https://polizasonline.online/sura/",
+        pending: "https://polizasonline.online/sura/"
+      },
+      auto_return: "approved"
+    };
+
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
+
+    res.json({ id: result.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al crear la preferencia :(" });
+  }
+});
+
+
+export default app;
